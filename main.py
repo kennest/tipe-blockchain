c2d77d886575f4245f3c52f8a84d89c76fb730a4
matthias4217 <matthias.goffette@laposte.net>
2016-03-17 14:36:55 +0100

    Added files via upload

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 """
 
-Gestion réseau
-
 Gestion transaction
+    - Vérification identité (couple clef publique/clef privée)
+    - Échange de clés Diffie-Hellman
+    - Commande pour la faire
 
 Blockchain
+    - Vérifier avec autres noeuds
 
+Preuve de travail (minage)
+    - Échange de clés Diffie-Hellman
+    
 """
