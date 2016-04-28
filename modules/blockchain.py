5d9da14b85a08f5334e0fdc111d99cfab20f290e
matthias4217 <matthias4217@users.noreply.github.com>
2016-04-28 15:07:58 +0200

    Finished get_blockchain

--- modules/blockchain.py
+++ modules/blockchain.py
@@ -3,7 +3,16 @@ Module de gestion de la blockchain
 """
 
 
-def getblockchain(utilisateur):
+def get_blockchain(utilisateur):
     """ Récupère la blockchain d'un utilisateur sous forme de liste. """
-    with open(utilisateur + 'blockchain', 'r') as f:
-        f.readlines()
+
+    # Détermine le dernier bloc
+    blockchain = os.listdir(utilisateur + "/blockchain") # récupère la liste des blocs
+    for i in range(len(blockchain)):
+        blockchain[i] = int(blockchain[i])
+    m = max(blocs) # Dernier bloc de l'utilisateur
+
+    # Récupère le bloc
+    with open(utilisateur + '/blockchain/' + m, 'r') as f:
+        bloc = f.readlines()
+    return bloc
