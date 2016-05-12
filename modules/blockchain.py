359659eafcdc0e2c21b965a6b2cfb30e5a7a90fe
matthias4217 <matthias4217@users.noreply.github.com>
2016-05-12 14:08:21 +0200

    Création de fonctions

--- modules/blockchain.py
+++ modules/blockchain.py
@@ -5,24 +5,37 @@ Module de gestion de la blockchain
 import os
 
 
-def get_blockchain(utilisateur):
-    """ Récupère la blockchain d'un utilisateur sous forme de liste. """
+def get_id_last_bloc(utilisateur):
+    """ Récupère le numéro du dernier bloc d'un utilisateur."""
 
-    # Détermine le dernier bloc
-    blockchain = os.listdir(utilisateur + "/blockchain") # récupère la liste des blocs
+    # récupère la liste des blocs
+    blockchain = os.listdir(utilisateur + "/blockchain")
+
+    # Récupère la liste des numéros des blockchains
     for i in range(len(blockchain)):
         blockchain[i] = int(blockchain[i])
-    m = max(blocs) # Dernier bloc de l'utilisateur
+    m = max(blockchain) # Dernier bloc de l'utilisateur
 
+
+
+def get_bloc(utilisateur, i):
+    """Récupère le bloc i de l'utilisateur. Renvoie une liste de la forme
+    [< hash précédent >, < hash du bloc >, < timestamp >,
+    < transaction 1 >, < transaction 2 >, < transaction 3 >,
+    solde alice, solde bob, solde cedric,
+    solde dylan, solde etienne, solde fanny]
+    """
     # Récupère le bloc
-    with open(utilisateur + '/blockchain/' + m, 'r') as f:
+    with open(utilisateur + '/blockchain/' + i, 'r') as f:
         bloc = f.readlines()
-    for i in range(3): # Les trois premiers champs sont
+    for j in range(3): # Les trois premiers champs sont
                             # 1) hash précédent
                             # 2) hash du bloc
                             # 3) timestamp
-        bloc[i] = int(bloc[i])
+        bloc[j] = int(bloc[j])
+        
     # traitement des trois transactions
-    for i in range(3,6):
-        #....
+    for j in range(3,6):
+        t = bloc.split(" ; ")
+        bloc[j] = [int(k) for k in t]
     return bloc
