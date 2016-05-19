d0161719022b1af44ee4796284160556caf969df
matthias.goffette <matthias.goffette@b309-p05>
2016-05-19 14:16:20 +0200

    Mise a jour du module hash

--- modules/hash.py
+++ modules/hash.py
@@ -9,11 +9,13 @@ def genere_clefs(utilisateur):
     
     chaine = utilisateur + 'mot_de_passe' + str(timestamp)
     hash_obj = hashlib.sha256(chaine.encode('utf8'))
-    publique = int(hash_obj.hexdigest())
+    publique = int(hash_obj.hexdigest(), 16)
 
-    temp =(int(time.time())) % 42) + (int(time.time())) % 17)
+    temp =( int(time.time()) % 42) + ( int(time.time()) % 17)
     
     chainep = str(publique) + chaine + str(temp)
     hash_obj_p = hashlib.sha256(chainep.encode('utf8'))
-    privee = int(hash_obj_p.hexdigest())
+    privee = int(hash_obj_p.hexdigest(), 16)
     return publique, privee
+
+
