512b9503d947078f683bcb2054a54ec13ded8542
matthias <matthias.goffette@b309-p05>
2016-05-26 14:42:12 +0200

    Création de alicetest, déboguage.

--- modules/pow.py
+++ modules/pow.py
@@ -16,14 +16,14 @@ import blockchain
 def gettransactions(utilisateur):
     """ Récupère les transactions en provenance d'un utilisateur. """
     liste = [] # liste de transactions
-    transactions = os.listdir("../utilisateurs" + utilisateur + "/transactions_pending")
+    transactions = os.listdir("../utilisateurs/" + utilisateur + "/transactions_pending")
           # liste des fichiers de transactions
     n = len(transactions)
     for i in range(max(n,3)): # Ne prend que trois transactions
-        with open(transactions[i], 'r') as f: # lecture de chaque fichier de transaction
+        with open("../utilisateurs/" + utilisateur + "/transactions_pending/" + transactions[i], 'r') as f: # lecture de chaque fichier de transaction
             t = f.readlines() # liste contenant la transaction
         liste.append(t)
-        os.remove(transactions[i]) # supprime le fichier de transaction
+        os.remove("../utilisateurs/" + utilisateur + "/transactions_pending/" + transactions[i]) # supprime le fichier de transaction
                                    # pour éviter des doublons
     return liste
 
@@ -36,6 +36,7 @@ def verifie_solde_transaction(utilisateur, i):
     id_last = blockchain.get_id_last_bloc(utilisateur)
     bloc = blockchain.get_bloc(utilisateur, id_last)
     l_bloc = len(bloc)
+    
     for i in range(6, l_bloc):
         clefpublique = bloc[i][0]
         solde = bloc[i][1]
@@ -48,4 +49,3 @@ def verifie_solde_transaction(utilisateur, i):
         if balance + solde < 0:
             return False
     return True
-
