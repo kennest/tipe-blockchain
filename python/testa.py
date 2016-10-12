ced38b1c9b5c770b184a4e2dd77f50302129a3ed
Matthias <matthias.goffette@gmail.com>
2016-10-12 15:07:39 +0200

    Continuation de testa

--- python/testa.py
+++ python/testa.py
@@ -1,6 +1,7 @@
 import modelisation as mod
 
 
+## Init
 net = mod.Reseau()
 
 for i in range(10):
@@ -10,4 +11,9 @@ liens = [(1,5),(1,2), (2,1),(2,0), (3,4),(3,5),(3,8),(3,9), (4,0), \
          (5,2),(5,6), (6,3), (7,3),(7,9), (8,3), (9,0),(9,7)]
 for lien in liens:
     net._set_tunnel(lien[0], lien[1])
+
+## Boucle principale
+    information0 = mod.Informations()
+    agent0 = net._get_agent(0)
+    agent0._add_information
 
