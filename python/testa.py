5a2cbdd02e5edeacd26246ff315ffbe4ee10af93
Matthias <matthias.goffette@gmail.com>
2016-10-19 15:13:41 +0200

    testa.py:  * rectification de liens  * commencé à commenter

--- python/testa.py
+++ python/testa.py
@@ -6,8 +6,8 @@ net = mod.Reseau()
 for i in range(10):
     net._set_agent(i)
 
-liens = [(1,5),(1,2), (2,1),(2,0), (3,4),(3,5),(3,8),(3,9), (4,0), \
-         (5,2),(5,6), (6,3), (7,3),(7,9), (8,3), (9,0),(9,7)]
+liens = [(0,3),(0,4), (1,2),(1,5), (2,0),(2,1), (3,4),(3,5),(3,8),(3,9), \
+         (4,0), (5,1),(5,2),(5,6), (6,3), (7,3),(7,9), (8,3), (9,0),(9,7)]
 for lien in liens:
     net._set_tunnel(lien[0], lien[1])
 
@@ -21,6 +21,7 @@ def envoyer_info(information, emetteur_id, dest_id):
     ag_dest._add_info(information)
     
 def actions_agent(agent):
+    """Réalise les actions entre les agents, depuis l'agent 'agent'."""
     ag_id = agent.id
     voisins_id = net._get_voisins(ag_id)
     for info in agent.informations:
