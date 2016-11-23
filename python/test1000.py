1c81050c8139794e09b3003b626c06d6cb83a523
matthias4217 <matthias.goffette@laposte.net>
2016-11-23 14:28:44 +0100

    Initialisation du réseau 1000

--- python/test1000.py
+++ python/test1000.py
@@ -1,4 +1,5 @@
 import modelisation as mod
+import random
 
 ## Init
 
@@ -6,3 +7,10 @@ net = mod.Reseau()
 
 for i in range(1000):
     net._set_agent(i)
+
+for i in range(1000):
+    voisins = [random.randrange(0,1000) for j in range(10)]
+    voisins = list(set(voisins))
+    for vois in voisins:
+        net._set_tunnel(i, vois)
+
