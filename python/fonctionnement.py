10d4b2429680c3d1235ec5bdd2c5a45f1a145224
Matthias <matthias.goffette@gmail.com>
2017-01-18 14:48:14 +0100

    Ajout d'une fonction de diffusion dans un réseau en étoile, avec des attaquants

--- python/fonctionnement.py
+++ python/fonctionnement.py
@@ -10,6 +10,8 @@ def boucle_diffusion(net, agent0, info):
     agent0 : int
     info : Information()
     """
+    ##A faire
+    #Modifier pour intégrer le comportement d'un attaquant
     
     ##Initialisation :
     #L'id de l'émetteur est ajoutée à l'info, et l'info est ajoutée à
@@ -17,15 +19,40 @@ def boucle_diffusion(net, agent0, info):
     info._add_passeur(agent0)
     net._get_agent(agent0)._add_info(info)
     
+    ##Diffusion vers les voisins
     list_vois = net._get_voisins_emet(agent0)
     for i in list_vois:
         
+        agent_i = net._get_agent(i)
         # Si l'agent ne possède pas encore l'information :
-        if not( info.id in net._get_agent(i)._get_list_info_id()):
-            
-            #copie pour éviter les problèmes d'alias sur la liste
-            #des destinataires
-            infobis = info.copy()
-            #par récurrence, la diffusion est effectuée
-            boucle_diffusion(net, i, infobis) 
+        if not( info.id in agent_i._get_list_info_id()):
+            if agent_i.strategie == "normal":
+                #copie pour éviter les problèmes d'alias sur la liste
+                #des destinataires
+                infobis = info.copy()
+                #par récurrence, la diffusion est effectuée
+                boucle_diffusion(net, i, infobis)
+            elif agent_i.strategie == "attaque":
+                infobis = info.copy()
+                infobis.infotxt = "faux"
+                boucle_diffusion(net, i, infobis)
 
+def diff_etoile(n, p, centre, emetteur, destinataire):
+    """ Génère un réseau en étoile de taille n avec p attaquants. (p<n)
+    Si centre = True, l'agent 0 sera un attaquant. Sinon, c'est un agent normal."""
+    
+    ##Initialisation
+    net = md.reseau_etoile(n)
+    if centre:
+        for i in range(p):
+            net._get_agent(i).strategie = "attaque"
+    else:
+        for i in range(1,p+1):
+            net._get_agent(i).strategie = "attaque"
+    
+    info = md.Information(0, destinataire, "vrai")
+    boucle_diffusion(net, emetteur, info)
+    
+    for i in range(n):
+        print("  ----  ")
+        net._get_agent(i).prinfo()
