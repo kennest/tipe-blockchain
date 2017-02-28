223990fc4564f178b8c378e841a92671c800f3c3
Matthias <matthias.goffette@gmail.com>
2017-02-28 19:02:39 +0100

    Correction de l'issue ? sur la gÃnÃration du réseau aléatoire : ne pas faire de tunnel vers soi-même.

--- python/modelisation/__init__.py
+++ python/modelisation/__init__.py
@@ -315,15 +315,15 @@ def reseau_complet(n):
                 net._set_tunnel_double(i,j)
     return net
 
-def gen_ens_aleat(n,m,M):
-    """Génère un ensemble à n éléments, compris entre m (inclus) et M (exclus)."""
+def gen_ens_aleat(n, m, M, y):
+    """Génère un ensemble à n éléments, compris entre m (inclus) et M (exclus), diff�rents de x."""
     if M-m < n:
         raise IntervalError("""Les bornes de l'intervalle sont trop rapprochées
         par rapport au nombre d'éléments""")
     ens = []
     while len(ens) < n:
         x = random.randint(m, M-1)
-        if not (x in ens):
+        if not (x in ens) and x != y:
             ens.append(x)
     return ens
 
@@ -333,7 +333,7 @@ def reseau_aleatoire(n,p):
     """Génère un réseau de taille n, avec chaque agent lié à p autres."""
     net =reseau_sans_tunnel(n)
     for i in range(n):
-        voisins = gen_ens_aleat(p, 0, n)
+        voisins = gen_ens_aleat(p, 0, n, i)
         for vois in voisins:
             net._set_tunnel(i, vois)
             
